2921194b6500e8eb25aebb6a7bc06c3aafb9ed52
Sylvain DUHANT <sylvain.duhant@condorcet.be>
2018-11-29 12:59:19 +0100

    Ajout d'un singleton de connexion

--- src/be/duhant/projet/DAO.java
+++ src/be/duhant/projet/DAO.java
@@ -1,28 +1,12 @@
 package be.duhant.projet;
 
-import java.sql.Connection;
 import java.sql.Statement;
-
-import javax.swing.JOptionPane;
-
-import oracle.jdbc.pool.OracleDataSource;
 
 public abstract class DAO<T> {
 	public abstract T Find(int id);
 	public abstract int add(T obj);
 	public abstract int getID();
 	public Statement connection() {
-		try {
-			OracleDataSource ods = new OracleDataSource();
-		    ods.setURL("jdbc:Oracle:thin:@char-oracle11.condorcet.be:1521:xe");
-		    ods.setUser("ORA26");
-		    ods.setPassword("b");
-		    Connection co = ods.getConnection();
-		    return co.createStatement();
-		}catch(Exception err) {
-			JOptionPane.showMessageDialog(null,err);
-			return null;
+			return SingletonDAO.instanciate().getStatement();
 		}
-		
 	}
-}
---
+++ src/be/duhant/projet/SingletonDAO.java
@@ -0,0 +1,58 @@
+package be.duhant.projet;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+
+import oracle.jdbc.pool.OracleDataSource;
+
+public class SingletonDAO {
+	private static boolean instanciated;
+	private static SingletonDAO instance;
+	private Statement stmt;
+	private Connection co;
+	
+	private SingletonDAO() {
+		try {
+			OracleDataSource ods = new OracleDataSource();
+		    ods.setURL("jdbc:Oracle:thin:@char-oracle11.condorcet.be:1521:xe");
+		    ods.setUser("ORA26");
+		    ods.setPassword("b");
+		    co = ods.getConnection();
+		    stmt = co.createStatement();
+		}catch(Exception err) {
+			JOptionPane.showMessageDialog(null,err);
+		}
+	}
+	
+	public Statement UpdateConnection() {
+		try {
+			stmt.close();
+			co.close();
+			OracleDataSource ods = new OracleDataSource();
+		    ods.setURL("jdbc:Oracle:thin:@char-oracle11.condorcet.be:1521:xe");
+		    ods.setUser("ORA26");
+		    ods.setPassword("b");
+		    co = ods.getConnection();
+		    stmt = co.createStatement();
+		    return stmt;
+		}
+		catch(Exception err){
+			JOptionPane.showMessageDialog(null,err);
+			return null;
+		}
+	}
+	public static SingletonDAO instanciate(){
+		if(!instanciated) {
+			return new SingletonDAO();
+		}
+		else {
+			instance.UpdateConnection();
+			return instance;
+		}
+	}
+	public Statement getStatement() {
+		return stmt;
+	}
+}
