554a1ad6c2c6e115bab7dd7a42b55fcd0e7ddd1c
Sylvain DUHANT <sylvain.duhant@condorcet.be>
2018-11-29 16:30:02 +0100

    Correction de bug mineure lors de la création d'une copie d'un jeu (test termines, valide pour la branche master)

--- src/be/duhant/projet/GameUser.java
+++ src/be/duhant/projet/GameUser.java
@@ -45,16 +45,20 @@ public class GameUser {
 		if(available) {
 			List<Order> conflict = Order.findOrder(ga);
 			Iterator<Order> it2 = conflict.iterator();
-			int a = 0;;
+			int a = 0;
 			if(conflict.size() > 0) {
+				while(it2.hasNext()) {
+					Order tmp = it2.next();
+					if(tmp.getPl().getUnit()>a) {
+						a=tmp.getPl().getUnit();
+					}
+				}
+				it2 = conflict.iterator();
 				while(it2.hasNext()) {
 					available = false;
 					Order tmp = it2.next();
-					if(a >= tmp.getPl().getUnit()) {
+					if(a > tmp.getPl().getUnit()) {
 						it2.remove();
-					}
-					else {
-						a = tmp.getPl().getUnit();
 					}
 				}
 				if(conflict.size()==1) {
@@ -65,12 +69,16 @@ public class GameUser {
 				else {
 					it2 = conflict.iterator();
 					Date dt = it2.next().getRegisterDate();
-					while(it2.hasNext()) {
+					while(it2.hasNext()) { //get the biggest
+						Order tmp = it2.next();
+						if(tmp.getRegisterDate().compareTo(dt) <= 0) {
+							dt=tmp.getRegisterDate();
+						}
+					}
+					it2 = conflict.iterator();
+					while(it2.hasNext()) { //delete the onces smaller than the biggest
 						if(it2.next().getRegisterDate().compareTo(dt) >= 0) {
 							it2.remove();
-						}
-						else {
-							dt = it2.next().getRegisterDate();
 						}
 					}
 					if(conflict.size()==1) {
@@ -82,51 +90,58 @@ public class GameUser {
 						it2 = conflict.iterator();
 						dt = it2.next().getPl().getRegisterDate();
 						while(it2.hasNext()) {
+							Order tmp = it2.next();
+							if(tmp.getPl().getRegisterDate().compareTo(dt) <= 0) {
+								dt = tmp.getPl().getRegisterDate();
+							}
+						}
+						it2 = conflict.iterator();
+						while(it2.hasNext()) {
 							if(it2.next().getPl().getRegisterDate().compareTo(dt) >= 0) {
 								it2.remove();
 							}
-							else {
-								dt = it2.next().getPl().getRegisterDate();
-							}
-							if(conflict.size()==1) {
-								dao.changeAvailability(false, this);
-								it2 = conflict.iterator();
-								it2.next().UpdateOrder(this);
-							}
-							else {
-								it2 = conflict.iterator();
-								dt = it2.next().getPl().getBirthday();;
-								while(it2.hasNext()) {
-									if(it2.next().getPl().getBirthday().compareTo(dt) >= 0) {
-										it2.remove();
-									}
-									else {
-										dt = it2.next().getPl().getBirthday();
-									}
-									if(conflict.size()==1) {
-										dao.changeAvailability(false, this);
-										it2 = conflict.iterator();
-										it2.next().UpdateOrder(this);
-									}
-									else {
-										dao.changeAvailability(false, this);
-										it2 = conflict.iterator();
-										Random r = new Random();
-										int result = r.nextInt(conflict.size());
-										for(int i = 0;i<result-1;i++) {
-											it2.next();
-										}
-										it2.next().UpdateOrder(this);
-									}
+						}
+						if(conflict.size()==1) {
+							dao.changeAvailability(false, this);
+							it2 = conflict.iterator();
+							it2.next().UpdateOrder(this);
+						}
+						else {
+							it2 = conflict.iterator();
+							dt = it2.next().getPl().getBirthday();
+							while(it2.hasNext()) {
+								Order tmp = it2.next();
+								if(tmp.getPl().getBirthday().compareTo(dt) <= 0) {
+									dt = tmp.getPl().getBirthday();
 								}
 							}
-						}
+							it2 = conflict.iterator();
+							while(it2.hasNext()) {
+								if(it2.next().getPl().getBirthday().compareTo(dt) >= 0) {
+									it2.remove();
+								}
+							}
+							if(conflict.size()==1) {
+							dao.changeAvailability(false, this);
+							it2 = conflict.iterator();
+							it2.next().UpdateOrder(this);
+							}
+							else {
+								dao.changeAvailability(false, this);
+								it2 = conflict.iterator();
+								Random r = new Random();
+								int result = r.nextInt(conflict.size());
+								for(int i = 0;i<result-1;i++) {
+									it2.next();
+								}
+									it2.next().UpdateOrder(this);
+								}
+							}
+						}						
 					}
 				}
 			}
 		}
-		
-	}
 	
 	public void changeAvailability(boolean b) {
 		dao.changeAvailability(b,this);
